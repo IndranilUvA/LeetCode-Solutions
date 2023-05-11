bef04ec6a8cd7d21565ed3b62a2cc0c48ad1f729
IndranilUvA <58029329+IndranilUvA@users.noreply.github.com>
2023-05-11 23:46:07 +0200

    Time: 184 ms (81.98%), Space: 16.7 MB (34.99%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        """
+        This problem is similar to longest common substring
+        """
+        m,n = len(nums1), len(nums2)
+        m,n = len(nums1), len(nums2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        
+        for i in range(m):
+            for j in range(n):
+                if nums1[i] == nums2[j]:
+                    dp[i+1][j+1] = dp[i][j] + 1
+                else:
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+        
+        return dp[-1][-1]
