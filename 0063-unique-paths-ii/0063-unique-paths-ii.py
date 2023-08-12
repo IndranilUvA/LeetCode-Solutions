f346a6d1261cebf2fe2baf2900a2158d3180bdd7
Indranil Bhattacharya <58029329+IndranilUvA@users.noreply.github.com>
2023-08-12 15:32:56 +0200

    Time: 59 ms (40.02%), Space: 16.5 MB (58.00%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,5 +1,13 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        
+        """
+        We create a dp matrix, dp[i][j] is the answer for i,jth cell.
+        Now, dp[0][0] is initiated based on the original 0,0th element
+        The other dp[i][j]
+        
+        """
+        
         nrow, ncol = len(obstacleGrid), len(obstacleGrid[0])
         dp = [[0]*ncol for _ in range(nrow)]
         if obstacleGrid[0][0] == 0:
@@ -13,7 +21,7 @@ class Solution:
                     if obstacleGrid[i][j] == 1:
                         dp[i][j] = 0
                     else:
-                        dp[i][j] = dp[i][j] + dp[i-1][j] + dp[i][j-1]
+                        dp[i][j] = dp[i-1][j] + dp[i][j-1]
         print(dp)
         return dp[-1][-1]
         
