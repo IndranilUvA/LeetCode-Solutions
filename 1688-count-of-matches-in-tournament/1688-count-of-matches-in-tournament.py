ae08ce64626a2d6fbd3e714997e38bbb0b047d82
Indranil Bhattacharya <58029329+IndranilUvA@users.noreply.github.com>
2023-12-05 23:50:09 +0100

    Time: 37 ms (59.39%), Space: 16.2 MB (71.69%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -0,0 +1,7 @@
+class Solution:
+    """
+    If one team is winner then n-1 team loses and it requires n-1 matches
+    """
+    def numberOfMatches(self, n: int) -> int:
+        return n-1
+        
